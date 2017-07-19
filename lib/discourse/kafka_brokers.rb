ac47b3bb0f56f7058352b4aea7fb88601bca9294
Col Perks <wild.fauve@gmail.com>
2017-07-20 10:13:55 +1200

    add the zookeeper kafka lookup back

--- lib/discourse/kafka_brokers.rb
+++ lib/discourse/kafka_brokers.rb
@@ -3,15 +3,15 @@ module Discourse
   class KafkaBrokers
 
     def call
-      M::Maybe(configuration.config.kafka_broker_list.split(","))
-      # zookeeper.()
+      # M::Maybe(configuration.config.kafka_broker_list.split(","))
+      zookeeper.()
     end
 
     private
 
-    # def zookeeper
-    #   Container['zookeeper_discovery']
-    # end
+    def zookeeper
+      Container['zookeeper_discovery']
+    end
 
     def configuration
       Container["configuration"]
